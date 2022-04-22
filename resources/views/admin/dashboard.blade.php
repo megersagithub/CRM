284bfe31eb336e9ae37d41801ab5a43732f7a583
megersagithub <megersa.daraje@astu.edu.et>
2022-04-22 09:08:06 +0300

    my first commit

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.master')
+
+@section('title')
+
+CRM_and_Store
+
+@endsection
+
+@section('content')
+
+{{-- <div class="row">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header">
+          <h4 class="card-title"> Simple Table</h4>
+        </div>
+        <div class="card-body">
+          <div class="table-responsive">
+            <table class="table">
+              <thead class=@extends('layouts.master') --}}
+
+              
+              
+             
+              
+              <div class="row">
+                  <div class="col-md-12">
+                    <div class="card">
+                      <div class="card-header">
+                        <h4 class="card-title"> Simple Table</h4>
+                      </div>
+                      <div class="card-body">
+                        <div class="table-responsive">
+                          <table class="table">
+                            <thead class=" text-primary">
+                              <th> Name </th>
+                              <th>  Email  </th>
+                              <th>  Update</th>
+                              <th > Delete </th>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <td> Dakota Rice</td>
+                                <td>   Dakota Rice  </td>
+                                <td>  Dakota Rice</td>
+                                <td> Dakota Rice </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+              @endsection
+              
+              @section('scripts')
+              
+              
+              @endsection
+              
+             
+
+
+
+
