dc533c05bc54ae3cc625395034f50bcb22c68c4b
megersagithub <megersa.daraje@astu.edu.et>
2022-04-23 07:13:30 +0300

    homeedited

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -64,7 +64,7 @@
 						</div>
 						<div class="mainmenu pull-left">
 							<ul class="nav navbar-nav collapse navbar-collapse">
-								<li><a href="index.html" class="active">Home</a></li>
+								<li><a href="#" class="active">Home</a></li>
 								<li class="dropdown"><a href="#">cart<i class="fa fa-angle-down"></i></a>
                                     
                                 </li> 
